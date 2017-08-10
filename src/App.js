e223d389a030dc5f01b3180b87f874bd4270cbb4
DKateryna <derkach.kateryna@gmail.com>
2017-08-10 23:30:42 +0200

    Added button to show student

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AddBatch from './components/AddBatch';
 import BatchesList from './components/BatchesList';
--- src/components/Batch.js
+++ src/components/Batch.js
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 
 
  class Batch extends Component {
+   constructor(){
+     super();
+     this.state = {showStudents: false};
+   }
+
+   showStudents(){
+     if(!this.state.showStudents) return '';
+     const studentsList = this.props.students.map((student) =>
+      <li>
+        {student.name}
+      </li>
+      )
+
+     return (
+       <ol>
+          {studentsList}
+       </ol>
+     )
+   }
+
+   toggleStudentList(){
+     this.setState({
+       ...this.state,
+       showStudents: !this.state.showStudents
+     })
+   }
+
    render(){
      return(
        <li>
          Batch #{this.props.number} from: {this.props.startDate} to: {this.props.endDate}
-         <button onClick={() => this.props.addStudent(this.props.number, {name: 'Joen'})}>Add Student</button>
+          students in the batch {this.props.students.length}
+           <button onClick={() => this.props.addStudent(this.props.number, {name: 'Joen'})}>
+            Add Student
+          </button>
+          <button onClick={() => this.toggleStudentList()}>Show students</button>
+          {this.showStudents()}
        </li>
      )
    }
