b99b2391b16dedd015e34b79d6690067b2592d2e
DKateryna <derkach.kateryna@gmail.com>
2017-08-11 00:18:44 +0200

    can add student with name

--- src/components/Batch.js
+++ src/components/Batch.js
@@ -4,7 +4,43 @@ import React, { Component } from 'react';
  class Batch extends Component {
    constructor(){
      super();
-     this.state = {showStudents: false};
+     this.state = {
+       showStudents: false,
+       showStudentInput: false
+     };
+
+    this.setName = this.setName.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+   }
+
+   handleSubmit(event){
+    this.props.addStudent(this.props.number, {name: this.state.name});
+    this.setState({...this.state, name: ''});
+    event.preventDefault();
+   }
+
+   setName(event){
+     this.setState({...this.state, name: event.target.value});
+   }
+
+   showStudentInput(){
+     if(!this.state.showStudentInput) return '';
+     return(
+       <form onSubmit={this.handleSubmit}>
+         <label>
+           Name:
+           <input type="text" name="name" value={this.state.name} onChange={this.setName} />
+         </label>
+         <input type="submit" value="Submit" />
+       </form>
+     )
+   }
+
+   toggleStudentInput(){
+     this.setState({
+       ...this.state,
+       showStudentInput: !this.state.showStudentInput
+     })
    }
 
    showStudents(){
@@ -34,10 +70,9 @@ import React, { Component } from 'react';
        <li>
          Batch #{this.props.number} from: {this.props.startDate} to: {this.props.endDate}
           students in the batch {this.props.students.length}
-           <button onClick={() => this.props.addStudent(this.props.number, {name: 'Joen'})}>
-            Add Student
-          </button>
+          <button onClick={() => this.toggleStudentInput()}>Add Student</button>
           <button onClick={() => this.toggleStudentList()}>Show students</button>
+          {this.showStudentInput()}
           {this.showStudents()}
        </li>
      )
